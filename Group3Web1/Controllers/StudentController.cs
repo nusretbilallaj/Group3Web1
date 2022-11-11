53750cf1b9e27217fc73b4ab92e7f839ddfbb7cc
Nusret Bilallaj <netktit@gmail.com>
2022-11-11 18:58:35 +0100

    Add project files.

---
+++ Group3Web1/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group3Web1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ParaqiteTopStudentin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Group3Web1/Models/Studenti.cs
@@ -0,0 +1,18 @@
+﻿namespace Group3Web1.Models
+{
+    public class Studenti
+    {
+        public Studenti(string emri,string mbiemri)
+        {
+            Emri = emri;
+            Mbiemri = mbiemri;
+        }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+
+        public string SayHello()
+        {
+            return $"Hello student {Emri}, {Mbiemri}";
+        }
+    }
+}
